f7143c75c5c11aaeec86f81ef05dc12b1e51c19a
pragatideovanshi <42382336+pragatideovanshi@users.noreply.github.com>
2023-05-04 20:59:24 +0530

    Javascript

---
+++ javascript1.js
@@ -0,0 +1,29 @@
+console.log("hello World")
+let f="javascript classes";
+console.log(f);
+let jv="abc";
+console.log( typeof "abc");
+console.log(typeof jv);
+let a=10;
+a="sbcsnbcn"
+console.log(a)
+console.log(typeof a)
+let a1;
+console.log(a1,a)
+let f1="pragati";
+let h="deovanshi";
+console.log(f1+" "+h)
+let firstname="Pragati";
+let jhonas="I'm"+firstname;
+console.log(jhonas);
+let jhonas1=`I'm${firstname}`;
+console.log(jhonas1);
+console.log("String with \n\ multiple \n\ lines")
+console.log(String("23"),23)
+let y="45";
+console.log(y)
+console.log(23+55)
+console.log("67"+"89");
+let fav=prompt("Number");
+console.log(fav);
+console.log(typeof(fav))
